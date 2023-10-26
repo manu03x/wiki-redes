95f49e81f9fc4b435aefa671dcabcea1c3687c6d
Brandon Lozano <blozano0@ucol.mx>
2023-10-26 17:49:35 -0600

    add concepto

--- js/db.js
+++ js/db.js
@@ -4,4 +4,64 @@ const conceptos = [
       "significado": "SIGNIFICADO TEST",
       "img": "https://concepto.de/wp-content/uploads/2018/10/URL1-e1538664726127.jpg"
     },
+    {
+      "concepto": "Red Social",
+      "significado": "Las redes sociales son plataformas en línea que permiten a las personas crear perfiles, conectarse con otros usuarios y compartir contenido, como texto, imágenes y videos. Estas plataformas son utilizadas para interactuar con amigos y familiares, seguir a celebridades, empresas y organizaciones, así como participar en discusiones y compartir experiencias personales.",
+      "img": "https://concepto.de/wp-content/uploads/2014/08/redes-sociales-1-e1551307320533.jpg"
+    },
+    {
+      "concepto": "Marketing en Redes Sociales",
+      "significado": "El marketing en redes sociales es el proceso de promocionar productos o servicios a través de plataformas de redes sociales. Esto implica la creación de contenido atractivo y relevante, la interacción con la audiencia, la publicidad dirigida y el seguimiento de métricas para alcanzar objetivos de marketing.",
+      "img": "https://www.sneakerlost.es/hubfs/Como-crear-una-estrategia-de-marketing-en-redes-sociales-01.png"
+    },
+    {
+      "concepto": "Branding",
+      "significado": "El branding se refiere a la gestión de la identidad de una marca, que incluye elementos como el logotipo, el eslogan y los valores de la empresa. El objetivo del branding es crear una imagen distintiva y memorable que los consumidores puedan identificar y confiar.",
+      "img": "https://static.semrush.com/blog/uploads/media/6a/97/6a9765594732531be7baab1c4a44e24c/branding-corporativo-logotipos.png"
+    },
+    {
+      "concepto": "CRM",
+      "significado": "El CRM (Customer Relationship Management) es una estrategia y tecnología que las empresas utilizan para administrar y mejorar sus relaciones con los clientes. Esto implica la recopilación de datos, el seguimiento de interacciones y la personalización de la experiencia del cliente para construir lealtad y satisfacción.",
+      "img": "https://www.cxtoday.com/wp-content/uploads/2022/01/CRM-101-Customer-Relationship-Management.jpeg"
+    },
+    {
+      "concepto": "Estrategias de marketing digital",
+      "significado": "Las estrategias de marketing digital son planes y tácticas diseñadas para promocionar productos o servicios en línea. Esto incluye publicidad en motores de búsqueda, redes sociales, correo electrónico y otros canales digitales.",
+      "img": "https://www.ecommercenews.pe/wp-content/uploads/2022/12/Marketing-Digital.jpg"
+    },
+    {
+      "concepto": "Buyer Persona",
+      "significado": "Un buyer persona es una representación semificticia del cliente ideal de una empresa. Se basa en datos demográficos, psicográficos y de comportamiento del cliente, y ayuda a las empresas a comprender mejor a quiénes se dirigen con sus productos o servicios.",
+      "img": "https://www.rdstation.com/blog/wp-content/uploads/sites/2/2022/10/Ejemplo-brand-persona-blog-rd-station-720x531.jpg"
+    },
+    {
+      "concepto": "Redes sociales para Pymes",
+      "significado": "Las redes sociales para pequeñas y medianas empresas (Pymes) son plataformas en línea que permiten a las Pymes promocionar sus productos o servicios, interactuar con clientes y aumentar su visibilidad en línea. Estas redes ofrecen oportunidades de marketing y comunicación asequibles para las empresas más pequeñas.",
+      "img": "https://www.grid.cl/blog/wp-content/uploads/2019/02/redes-sociales-y-las-pymes.jpg"
+    },
+    {
+      "concepto": "Engagement",
+      "significado": "El engagement se refiere a la interacción y participación de los usuarios en las redes sociales con el contenido de una marca. Esto incluye acciones como dar Me gusta, comentar y compartir publicaciones, lo que contribuye a la construcción de una comunidad comprometida.",
+      "img": "https://webescuela.com/wp-content/uploads/2021/07/que-es-el-engagement.png.webp"
+    },
+    {
+      "concepto": "SEO",
+      "significado": "El SEO (Search Engine Optimization) es un conjunto de estrategias y técnicas utilizadas para mejorar la visibilidad de un sitio web en los resultados de los motores de búsqueda, como Google. Esto se logra mediante la optimización de palabras clave, la creación de contenido de calidad y la mejora de la estructura del sitio.",
+      "img": "https://nothingad.com/wp-content/uploads/2019/04/Que-es-el-posicionamiento-SEO-en-marketing-digital-1.jpg"
+    },
+    {
+      "concepto": "SEM",
+      "significado": "El SEM  (Search Engine Marketing) es una estrategia de marketing que implica la promoción de un sitio web a través de la publicidad en motores de búsqueda, como Google Ads. A diferencia del SEO, que se enfoca en el posicionamiento orgánico, el SEM se basa en la publicidad paga para aumentar la visibilidad en los resultados de búsqueda.",
+      "img": "https://irp-cdn.multiscreensite.com/355fb8b4/que-es-el-sem.jpg"
+    },
+    {
+      "concepto": "RSS",
+      "significado": "RSS (Really Simple Syndication) es un formato de alimentación web que permite a los usuarios recibir actualizaciones de contenido de sitios web y blogs de manera automatizada. Los usuarios pueden suscribirse a fuentes RSS para mantenerse al tanto de las últimas noticias y publicaciones sin tener que visitar los sitios web individualmente.",
+      "img": "https://www.mediagroup.es/wp-content/uploads/2016/06/rss.jpg"
+    },
+    {
+      "concepto": "CMS",
+      "significado": "Un CMS (Content Management System) es un sistema de gestión de contenido que facilita la creación, edición y administración de contenido en sitios web sin requerir conocimientos técnicos. Los CMS son ampliamente utilizados para simplificar la administración de sitios web y la publicación de contenido en línea.",
+      "img": "https://www.itconsultors.com/images/blog/CMS_c3xocc.jpg"
+    },
   ];
