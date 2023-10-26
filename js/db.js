51b62299f36f6188753ababbb19faa4543af3cce
Emmanuel Ruiz <eruiz3@ucol.mx>
2023-10-26 12:53:13 -0600

    Instrucciones y tests

--- js/db.js
+++ js/db.js
@@ -1,19 +1,7 @@
 const conceptos = [
     {
-      "concepto": "Array",
-      "significado": "Una estructura de datos que almacena una colección de elementos de datos del mismo tipo.",
+      "concepto": "TEST",
+      "significado": "SIGNIFICADO TEST",
       "img": "https://concepto.de/wp-content/uploads/2018/10/URL1-e1538664726127.jpg"
     },
-    {
-      "concepto": "Objeto",
-      "significado": "Una estructura de datos que almacena un grupo de propiedades y valores."
-    },
-    {
-      "concepto": "Función",
-      "significado": "Un bloque de código que se puede ejecutar repetidamente."
-    },
-    {
-      "concepto": "Variable",
-      "significado": "Un espacio en memoria que se utiliza para almacenar un valor."
-    }
   ];
